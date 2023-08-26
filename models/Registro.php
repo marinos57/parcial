195a9dab5ab07e5fed29008364dd0965e06875f9
marino5710 <brianmarin4@gmail.com>
2023-08-26 09:43:03 -0600

    se creo el modelo de registro.

---
+++ models/Registro.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Model;
+
+class Usuario extends ActiveRecord {
+    protected static $tabla = 'usuario';
+    protected static $columnasDB = ['USU_NOMBRE', 'USU_APELLIDO', 'USU_USUARIO', 'USU_PASSWORD', 'USU_SITUACION'];
+    protected static $idTabla = 'USU_ID';
+
+    public $usu_id;
+    public $usu_nombre;
+    public $usu_apellido;
+    public $usu_usuario;
+    public $usu_password;
+    public $usu_situacion;
+
+    public function __construct($args = []) {
+        $this->usu_id = $args['usu_id'] ?? null;
+        $this->usu_nombre = $args['usu_nombre'] ?? '';
+        $this->usu_apellido = $args['usu_apellido'] ?? '';
+        $this->usu_usuario = $args['usu_usuario'] ?? '';
+        $this->usu_password = $args['usu_password'] ?? '';
+        $this->usu_situacion = $args['usu_situacion'] ?? '';
+    }
+}
