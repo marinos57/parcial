9b00099bd1dc58cd280fa29fc854447de2410e52
marino5710 <brianmarin4@gmail.com>
2023-08-26 10:18:12 -0600

    se agregaron las rutas para registros

--- public/index.php
+++ public/index.php
@@ -4,10 +4,18 @@ require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
 use Controllers\AppController;
+use Controllers\RegistroController;
+
 $router = new Router();
 $router->setBaseURL('/' . $_ENV['APP_NAME']);
 
 $router->get('/', [AppController::class,'index']);
+
+
+//registros
+
+$router->get('/registros',[RegistroController::class, 'index']);
+$router->post('/API/registros/guardar', [RegistroController::class,'guardarApi']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
